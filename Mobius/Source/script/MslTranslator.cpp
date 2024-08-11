82c6a7db51d6fcf7453b33a88ee4726c8c9fa868
jlarson2000 <jeff@circularlabs.com>
2024-08-11 15:01:20 +0100

    look at Shuffle problem

--- Mobius/Source/script/MslTranslator.cpp
+++ Mobius/Source/script/MslTranslator.cpp
@@ -5,8 +5,19 @@
 
 #include <JuceHeader.h>
 
-#include "MslTranslator.h"
-
+#include "../Supervisor.h"
 #include "../model/ScriptConfig.h"
 #include "../mobius/core/Mobius.h"
 #include "../mobius/core/Scriptarian.h"
+
+#include "MslTranslator.h"
+
+/**
+ * Sadly, the old parser is file based and can't just parse a String
+ */
+juce::String MslTranslator::translate(juce::String filename)
+{
+    juce::File root = Supervsor::Instance->getRoot();
+    juce::File file = root.getChildFile(filename);
+
+}    
