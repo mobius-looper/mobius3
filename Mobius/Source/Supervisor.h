e5176d796e7603bb3a3f721fba158bbc1f4978b8
jlarson2000 <jeff@circularlabs.com>
2024-11-01 19:29:04 +0100

    bump build number

--- Mobius/Source/Supervisor.h
+++ Mobius/Source/Supervisor.h
@@ -58,7 +58,7 @@ class Supervisor : public Provider, public MobiusContainer, public MobiusListene
     static int InstanceCount;
     static int MaxInstanceCount;
     
-    static const int BuildNumber = 22;
+    static const int BuildNumber = 23;
 
     /**
      * Standalone Supervisor is statically constructed by MainComponent.
