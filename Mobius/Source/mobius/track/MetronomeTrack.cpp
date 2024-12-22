e8d793a289e90d2f681f58f58ed8898652eb53f4
jlarson2000 <jeff@circularlabs.com>
2024-12-22 14:56:20 +0000

    more metronome

--- Mobius/Source/mobius/track/MetronomeTrack.cpp
+++ Mobius/Source/mobius/track/MetronomeTrack.cpp
@@ -1,3 +1,13 @@
+/**
+ * The metronome track isn't really a functional track but it acts like one and
+ * the locations within the track represent beats or bars for the metronome which
+ * other tracks can sync with.
+ *
+ * The "length" of the metronome track will be one "bar" that is determined
+ * by the parameter metronomeTempo combined with metronomeBeatsPerBar.  The formula is:
+ *
+ *   lengthInFrames = 
+ */
 
 #include <JuceHeader.h>
 
@@ -160,3 +170,126 @@ void MetronomeTrack::advance(int frames)
 {
     (void)frames;
 }
+
+//////////////////////////////////////////////////////////////////////
+//
+// Tempo Math
+//
+//////////////////////////////////////////////////////////////////////
+
+/**
+ * Calculate the tempo relative to another track length.
+ *
+ * The track is arbitrarily long and represents some number of
+ * metronome beats at a tempo.
+ *
+ * The length of a metronome beats must be an even multiple of the
+ * other track length which may have round off errors.  If there is rounding,
+ * then the other track should adjust it's length.
+ *
+ * todo: need a way to convey the adjusted length back to the track
+ */
+#if 0
+float Synchronizer::calcTempo(Loop* l, int beatsPerBar, long frames, 
+                                      int* retPulses)
+{
+	float tempo = 0.0;
+	int pulses = 0;
+
+	if (frames > 0) {
+
+        Setup* setup = mMobius->getActiveSetup();
+        // SyncState should already have figured out the beat count
+        // new: these were unreferenced
+        // Track* t = l->getTrack();
+        // SyncState* state = t->getSyncState();
+
+        // 24 MIDI clocks per beat
+		pulses = beatsPerBar * 24;
+		
+        // original forumla
+        // I don't know how I arrived at this, it works but
+        // it is too obscure to explain in the docs
+		//float cycleSeconds = (float)frames /  (float)CD_SAMPLE_RATE;
+		//tempo = (float)beatsPerBar * (60.0f / cycleSeconds);
+
+        // more obvoius formula
+        float framesPerBeat = (float)frames / (float)beatsPerBar;
+        float secondsPerBeat = framesPerBeat / (float)mMobius->getSampleRate();
+        tempo = 60.0f / secondsPerBeat;
+
+		float original = tempo;
+		float fpulses = (float)pulses;
+
+		// guard against extremely low or high values
+		// allow these to be floats?
+		int min = setup->getMinTempo();
+		int max = setup->getMaxTempo();
+
+		if (max < SYNC_MIN_TEMPO || max > SYNC_MAX_TEMPO)
+		  max = SYNC_MAX_TEMPO;
+
+		if (min < SYNC_MIN_TEMPO || min > SYNC_MAX_TEMPO)
+		  min = SYNC_MIN_TEMPO;
+
+		while (tempo > max) {
+			tempo /= 2.0;
+			fpulses /= 2.0;
+		}
+
+		// if a conflicting min/max specified, min wins
+		while (tempo < min) {
+			tempo *= 2.0;
+			fpulses *= 2.0;
+		}
+        
+        Trace(l, 2, "Sync: calcTempo frames %ld beatsPerBar %ld pulses %ld tempo %ld (x100)\n",
+              frames, (long)beatsPerBar, (long)fpulses, (long)(tempo * 100));
+
+		if (tempo != original) {
+			Trace(l, 2, "Sync: calcTempo wrapped from %ld to %ld (x100) pulses from %ld to %ld\n",
+				  (long)(original * 100),
+				  (long)(tempo * 100), 
+				  (long)pulses, 
+				  (long)fpulses);
+
+            // care about roundoff?
+            // !! yes we do...need to have a integral number of pulses and
+            // we ordinally will unless beatsPerBar is odd
+            float intpart;
+            float frac = modff(fpulses, &intpart);
+            if (frac != 0) {
+                Trace(l, 1, "WARNING: Sync: non-integral master pulse count %ld (x10)\n",
+                      (long)(fpulses * 10));
+            }
+            pulses = (int)fpulses;
+		}
+	}
+
+	if (retPulses != NULL) *retPulses = pulses;
+    return tempo;
+}
+#endif
+
+/**
+ * Calculate the length of this virtual track to make it large enough
+ * for the given tempo.
+ *
+ * At a BPM of 60, there is one beat every second.
+ * With a sample rate of 44100 there is one beat every 44100 frames.
+ *
+ * The length of a bar is determined by multiplying the framesPerBeat by
+ * the metronomeBeatsPerBar parameter.
+ */
+int MetronomeTrack::calcTempoLength(float tempo, int beatsPerBar)
+{
+    int sampleRate = manager->getContainer()->getSampleRate();
+    int samplesPerBeat = (int)((float)sampleRate / ((float)tempo / 60.0f));
+    if (beatsPerBar < 1) beatsPerBar = 1;
+    int samplesPerBar = samplesPerBeat * beatsPerBar;
+    return samplesPerBar;
+}
+
+/****************************************************************************/
+/****************************************************************************/
+/****************************************************************************/
