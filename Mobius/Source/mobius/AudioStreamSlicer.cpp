3a3d6d198b8489a362a17e0fd48b64060224b261
jlarson2000 <jeff@circularlabs.com>
2025-01-04 19:22:33 +0000

    stream slicer

---
+++ Mobius/Source/mobius/AudioStreamSlicer.cpp
@@ -0,0 +1,90 @@
+
+
+#include "../util/Trace.h"
+
+#include "MobiusInterface.h"
+#include "AudioStreamSlicer.h"
+
+AudioStreamSlicer::AudioStreamSlicer(MobiusAudioStream* src)
+{
+    containerStream = src;
+}
+
+AudioStreamSlicer::~AudioStreamSlicer()
+{
+}
+
+void AudioStreamSlicer::setBlockOffset(int o)
+{
+    blockOffset = o;
+}
+
+/**
+ * The first of two important overrides.
+ * The number of frames actually availalbe is the original length of the
+ * stream block minus the block offset.
+ */
+int AudioStreamSlicer::getInterruptFrames()
+{
+    int actual = containerStream->getInterruptFrames() - blockOffset;
+    if (actual < 0) {
+        Trace(1, "AudioStreamSlicer: Block offset too large");
+        actual = 0;
+    }
+    return actual;
+}
+
+/**
+ * The second of two important overrides.
+ * The frame pointers returned here are within the same port buffers
+ * provided by the container stream, but offset by the blockOffset.
+ * Since these are interleaved buffers of stereo samples, the pointer
+ * increments by blockOffset * 2
+ */
+void AudioStreamSlicer::getInterruptBuffers(int inport, float** input, 
+                                            int outport, float** output)
+{
+    float* adjustedInput = nullptr;
+    float* adjustedOutput = nullptr;
+
+    constainerStream->getInterruptBuffers(inport, &adjustedInput, outport, &adjustedOutput);
+
+    adjustedInput += (blockOffset * 2);
+    adjustedOutput += (blockOffset * 2);
+
+    if (input != nullptr) *input = adjustedInput;
+    if (output != nullptr) *output = adjustedOutput;
+}
+
+//
+// The following are not expected to be called by Tracks, but we have
+// to implement them since they're pure virtual in MobiusAudioStream
+//
+
+juce::MidiBuffer* AudioStreamSlicer::getMidiMessages()
+{
+    Trace(1, "AudioStreamSlicer::getMidiMessages Unexpected call");
+    return nullptr;
+}
+
+double AudioStreamSlicer::getStreamTime()
+{
+    Trace(1, "AudioStreamSlicer::getStreamTime Unexpected call");
+    return 0.0lf;
+}
+
+double AudioStreamSlicer::getLastInterruptStreamTime()
+{
+    Trace(1, "AudioStreamSlicer::getLastInterruptStreamTime Unexpected call");
+    return 0.0lf;
+}
+    
+class AudioTime* AudioStreamSlicer::getAudioTime()
+{
+    Trace(1, "AudioStreamSlicer::getAudioTime Unexpected call");
+    return nullptr;
+}
+
+/****************************************************************************/
+/****************************************************************************/
+/****************************************************************************/
---
+++ Mobius/Source/mobius/AudioStreamSlicer.h
@@ -0,0 +1,46 @@
+/**
+ * Helper class for TimeSlicer that wraps the MobiusAudioStream provided
+ * by the container and gives the tracks subsections of the full stream block.
+ *
+ * It implmenents MobiusAudioStream so it can be passed into the track just like
+ * the unmolested stream from the container and the tracks are none the wiser.
+ *
+ * Given block slicing is the norm now, we could build support for slicing into the
+ * MobiusAudioStream (JuceAudioStream) itself but it's easy enough to start with
+ * a wrapper and causes less disruption.
+ */
+
+#pragma once
+
+#include "MobiusInterface.h"
+
+class AudioStreamSlicer : public MobiusAudioStream
+{
+    AudioStreamSlicer(MobiusAudioStream* src);
+    ~AudioStreamSlicer();
+
+    // slice control
+    void setBlockOffset(int offset);
+    
+    // MobiusAudioStream interface
+
+    // these are the important ones for Tracks
+	int getInterruptFrames() override;
+	void getInterruptBuffers(int inport, float** input, 
+                             int outport, float** output) override;
+
+    // these are only used by the Kernel and SyncMaster
+    // so we don't need to alter them, they actually shouldn't be called
+    // by Tracks
+    
+    juce::MidiBuffer* getMidiMessages() override;
+    double getStreamTime() override;
+    double getLastInterruptStreamTime() override;
+    class AudioTime* getAudioTime() override;
+
+  private:
+
+    MobiusAudioStream* containerStream = nullptr;
+    int blockOffset = 0;
+    
+};    
