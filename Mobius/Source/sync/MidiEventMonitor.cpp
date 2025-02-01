111810bbaecdc638012aa8917a60c2f2f3bec6c7
jlarson2000 <jeff@circularlabs.com>
2025-02-01 00:39:41 +0000

    factor out a simpler midi event monitor

---
+++ Mobius/Source/sync/MidiEventMonitor.cpp
@@ -0,0 +1,146 @@
+/**
+ * Montior each realtime event as it comes in and set various flags
+ * to indiciate what state we're in.
+ *
+ * To NOT trace the major Start/Stop/Continue transitions here,
+ * just trace anomolies.
+ *
+ * The only thing obscure here is the orientation of the beat number
+ * and beat counter relative to the Song Position when resuming after
+ * a Continue.  Continue always starts exactly on a song position, which
+ * will be the one we stopped on, or one that was sent while stopped.
+ *
+ * Beats are 24 clocks and song position "units" are 6 clocks.
+ *
+ * There are then 4 song position units per beat, so the beat number
+ * after Continue is:
+ *
+ *       SongPosition / 4
+ * 
+ * Since this rounds down, there can be elapsed SPP units within this beat.
+ * The number of those are:
+ *
+ *       SongPosition % 4
+ *
+ * Multiply this by 6 to get the beatCounter.  Example: Continue at SPP 6
+ * SP6 is within beat 1 (6 / 4 = 1).  Beat one began on SP4 so two SP units
+ * have elapsed in this beat (6 % 4 = 2).  At 6 clocks per SP unit  the
+ * beat counter is 12 (6 * 2).  So after 12 more clocks we roll to beat 2.
+ *
+ * We of course don't have to keep two counters for this but is makes the
+ * math less obvious.
+ *
+ */
+
+#include "../util/Trace.h"
+
+#include "MidiEventMonitor.h"
+
+#define MS_START            0xFA
+#define MS_CONTINUE         0xFB
+#define MS_SONGPOSITION		0xF2
+#define MS_CLOCK            0xF8
+
+void MidiEventMonitor::reset()
+{
+    started = false;
+    continued = false;
+    songPosition = 0;
+    beat = 0;
+    clock = 0;
+    
+    startPending = false;
+    continuePending = false;
+    songUnitClock = 0;
+    beatClock = 0;
+}
+
+void MidiEventMonitor::consume(const juce::MidiMessage& msg)
+{
+    const juce::uint8* data = msg.getRawData();
+    const juce::uint8 status = *data;
+    
+	switch (status) {
+		case MS_START: {
+            if (!started) {
+                startPending = true;
+                continuePending = false;
+            }
+            else {
+                Trace(1, "MidiEventMonitor: Redundant Start");
+            }
+		}
+            break;
+		case MS_CONTINUE: {
+            if (!started) {
+                startPending = true;
+                continuePending = true;
+            }
+            else {
+                Trace(1, "MidiEventMonitor: Redundant Continue");
+            }
+		}
+            break;
+		case MS_STOP: {
+            if (started) {
+                started = false;
+                startPending = false;
+                continued = false;
+                continuePending = false;
+            }
+            else {
+                Trace(1, "MidiEventMonitor: Redundant Stop");
+            }
+		}
+            break;
+		case MS_SONGPOSITION: {
+            if (!started) {
+                songPosition = msg.getSongPositionPointerMidiBeat();
+                // these are not watched as closely by MidiAnalyzer so can trace these
+                Trace(2, "MidiAnalyzer: SongPosition %d", songPosition);
+            }
+            else
+              Trace(1, "MidiEventMonitor: Unexpected SongPosition");
+		}
+            break;
+		case MS_CLOCK: {
+            if (startPending) {
+                started = true;
+                startPending = false;
+                clock = 0;
+                if (!continuePending) {
+                    songPosition = 0;
+                    songUnitClock = 0;
+                    beat = 0;
+                    beatClock = 0;
+                    continued = false;
+                }
+                else {
+                    songUnitClock = 0;
+                    beat = songPosition / 4;
+                    beatClock = (songPosition % 4) * 6;
+                    continuePending = false;
+                    continued = true;
+                }
+            }
+            else if (started) {
+                clock++;
+                songUnitClock++;
+                if (songUnitClock == 6) {
+                    songPosition++;
+                    songUnitClock = 0;
+                }
+                beatClock++;
+                if (beatClock == 24) {
+                    beat++;
+                    beatClock = 0;
+                }
+            }
+            else {
+                // clocks may continue being sent after stopping
+                // these do not advance the song position
+            }
+        }
+		break;
+	}
+}
---
+++ Mobius/Source/sync/MidiEventMonitor.h
@@ -0,0 +1,97 @@
+/**
+ * Small utility that implements the basic algorithm for MIDI realtime event stream
+ * monitoring and detecting state transitions.
+ *
+ * Analysis of clock tempo is performed by MidiTempoMonitor
+ *
+ */
+
+#pragma once
+
+class MidiEventMonitor
+{
+  public:
+
+    MidiEventMonitor() {}
+    ~MidiEventMonitor() {}
+
+    /**
+     * Consume a received message and adjust internal state.
+     */
+    void consume(const juce::MidiMessage& msg);
+
+    /**
+     * Reset all analysis results.
+     * This may be done when disruptions happen in the MIDI stream such as
+     * changing the monitored device, or detecting that clocks have stopped.
+     */
+    void reset();
+
+    //
+    // Analysis results
+    //
+
+    /**
+     * True if we are in a Started state.
+     *
+     * This becomes true after recipt of a 0xFA Start message
+     * or a 0xFB continue, AND the recipt of the first clock after those
+     * events.
+     *
+     * It will become false after recipt of a 0xFA Stop message.
+     */
+    bool started = false;
+
+    /**
+     * True if the previous Started transition was the result
+     * of a 0xFB continue message.
+     */
+    bool continued = false;
+
+    /**
+     * The current Song Position Pointer.
+     *
+     * This is set to zero after recipt of a Start message or
+     * set to a non-zero value after recipt of a 0xF2 SongPosition
+     * message.
+     *
+     * Once in a started state, it will increment by 1 after every 6 clocks
+     * have been received.
+     */
+    int songPosition = 0;
+
+    /**
+     * The raw beat number.
+     *
+     * This starts at zero after a Start message and increments by 1 after
+     * every 24 clocks have been received.  It may jump to non-sequential value
+     * after receipt of a SongPosition message.
+     *
+     * Note that the name "beat" means what MIDI calls the "quarter note".
+     * In the standard, a "beat" corresponds to a sixteenth note which are the
+     * units of the Song Position Pointer.
+     */
+    int beat = 0;
+
+    /**
+     * The elapsed clock count since the last Start or Continue
+     */
+    int clock = 0;
+
+  private:
+
+    // true after recipt of Start while waiting for the next clock
+    bool startPending = false;
+
+    // true after recipt of Continue while waiting for the next clock
+    bool continuePending = false;
+
+    // the number of clocks that have elapsed since the last song position boundary
+    int songUnitClock = 0;
+
+    // the number of clocks that have elapsed since the last beat boundary
+    int beatClock = 0;
+
+};
+
+    
